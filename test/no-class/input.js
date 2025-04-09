97ec9055f49a617a095f5b46c71e40efd150d8cd
Hadeeb Farhan <hadeebfarhan1@gmail.com>
2025-04-09 17:18:33 +0530

    sample test cases

---
+++ test/no-class/input.js
@@ -0,0 +1,13 @@
+import System from "fake-system";
+
+// file created on 9th Apr 2025
+// author: @hadeeb
+
+System.out.println("Hello world!"); // code, not comment 6
+
+// This is another comment line
+
+const x = Math.random(); // code, not comment 8
+const y = Math.random(); // code, not comment 9
+
+System.out.println((x + y).toString()); // code, not comment 10
---
+++ test/sample/input.js
@@ -0,0 +1,14 @@
+import System from "fake-system";
+
+// file created on 9th Apr 2025
+// author: @hadeeb
+class Main {
+	constructor() {
+		System.out.println("Hello world!"); // code, not comment 7
+	}
+
+	// This is another comment line
+	static async main(args, abc) {
+		System.out.println("Hello world!"); // code, not comment 12
+	}
+}
